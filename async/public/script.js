0be401ed62a12c560f3a2142a12de6063aa2b1d2
Matthias Kainer <mail@matthias-kainer.de>
2022-01-23 15:44:42 +0100

    Added error template example

--- async/public/script.js
+++ async/public/script.js
@@ -1,13 +1,13 @@
 import { html } from 'https://unpkg.com/lit@2.1.1/index.js?module';
-import { pureLit } from 'https://unpkg.com/pure-lit@2.0.1/lib/index.module.js?module';
+import { pureLit } from 'https://unpkg.com/pure-lit@2.0.2/lib/index.module.js?module';
 
-const url = 'https://jsonplaceholder.typicode.com/posts';
-
-pureLit('posts-list', async () => {
-  const data = await fetch(url)
+pureLit('posts-list', async (element) => {
+  const data = await fetch(element.src)
     .then((response) => response.json());
 
   return html`<ul>
     ${data.map((entry) => html`<li>${entry.title}</li>`)}
   </ul>`;
+}, {
+  defaults: { src: "" }
 });
